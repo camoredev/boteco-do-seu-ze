6e6d9a855011b6a831d432a43d2cc67f2e2ba759
Caio Moraes <camoredev@gmail.com>
2022-10-28 20:22:23 -0300

    conexão do front com back

--- botecodoseuze-fronted/src/services/services.js
+++ botecodoseuze-fronted/src/services/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://localhost:3040'});
+const api = axios.create({baseURL: 'https://backend-botecodoseuze-production.up.railway.app/'});
  
 export default api;
